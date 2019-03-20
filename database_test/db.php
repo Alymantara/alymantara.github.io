89072e6447cf45352b677348696b65700e80c2e3
J.V. Hernandez Santisteban <j.v.hernandez@uva.nl>
2019-03-20 15:07:05 +0100

    updated to alymantara.com

---
+++ database_test/db.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+
+$con = mysqli_connect("localhost","alymanta","2H9o3fv2iZ","alymanta_test");
+//$con = mysqli_connect("localhost","cv_tester","ftDsBIufK(z7","alymanta_test");
+ // Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+?>
---
+++ database_test2/db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+$con = mysqli_connect("localhost","alymanta","2H9o3fv2iZ","alymanta_test");
+ // Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+?>
